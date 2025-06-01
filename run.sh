70deeb76c2c3fe218d61caeb60b2ee03c4e69e29
kilisio <pkilisio@gmail.com>
2025-06-01 08:59:20 +0300

    include all hls audio

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+sudo rm /usr/local/bin/termv
 sudo wget -O /usr/local/bin/termv -q --show-progress https://raw.githubusercontent.com/kilisio/termv/main/termv 
 sudo chmod +x /usr/local/bin/termv
